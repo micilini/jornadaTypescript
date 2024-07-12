dc49daa9075b008c1df9b27153b0277d32464d26
Micilini <sdanz@hotmail.com>
2024-07-12 17:05:24 -0300

    Lição 1

---
+++ 1 - primeiro-projeto/index.js
@@ -0,0 +1,6 @@
+var nome = "Micilini";
+var rank = 100;
+function mostraDados(nome, rank) {
+    console.log("Nome: ".concat(nome, " - Rank: ").concat(rank));
+}
+mostraDados(nome, rank);
---
+++ 1 - primeiro-projeto/index.ts
@@ -0,0 +1,8 @@
+const nome = "Micilini";
+var rank = 100;
+
+function mostraDados(nome, rank){
+    console.log(`Nome: ${nome} - Rank: ${rank}`);
+}
+
+mostraDados(nome, rank);
